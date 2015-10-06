69685088b60b27ed76478a23a1316351460e62d4
csworen <swo13001@byui.edu>
2015-10-05 19:28:57 -0700

    Added dynamic_calendar_facing.html, render.js, calendar.css, and favicon.png It's all coming together. Someone make it prettier. =D

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #Group Project Readme File Example for Now
 This is a new line added by Adam
 I just added code to test. - Jordan Harmon
+I have just added the render.js (populates calendar inside a div with class/id = calendar), the calendar.css (styles day based on availability)
+	, and a slightly edited BYUI logo (favicon.png) for the browser tab logo (until we decide on a logo for the group. -C§
---
+++ js/render.js
@@ -0,0 +1,50 @@
+		function getCalendar() {
+			var now = new Date();
+			var thisMonth = now.getMonth();
+			var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+			var month = months[now.getMonth()];
+			var thisYear = now.getFullYear();
+			var firstDay = new Date(thisYear, thisMonth, 1, 0, 0, 0, 0);
+			var firstDayOfWeek = firstDay.addDays(-firstDay.getDay()); // get the first date of the 5-week calendar
+			var calendar = '<h1><label>' + month + ' &nbsp;&nbsp; ' + thisYear + '</label></h1> '
+				+ '<table class="table-bordered"> <tr> <td class="day text-center">Sun</td> <td class="day text-center">Mon</td> <td class="day text-center">Tue</td> <td class="day text-center">Wed</td> <td class="day text-center">Thu</td> <td class="day text-center">Fri</td> <td class="day text-center">Sat</td> </tr> ';
+			var calMonth = new Array(6); // least amount that will contain an entire month every time
+			calMonth[0] = new Array(7); // each week contains 7 days
+			calMonth[1] = new Array(7);
+			calMonth[2] = new Array(7);
+			calMonth[3] = new Array(7);
+			calMonth[4] = new Array(7);
+			calMonth[5] = new Array(7);
+			for(var i = 0; i < 6; i++) {
+				for(var j = 0; j < 7; j++) {
+					calMonth[i][j] = new Date(firstDayOfWeek); // input each date for the calendar into the array
+					firstDayOfWeek = new Date(firstDayOfWeek.addDays(1));
+				}
+			}
+			for(var i = 0; i < 6; i++) {
+				calendar += '<tr>';
+				for(var j = 0; j < 7; j++) {
+				var currDate = new Date(calMonth[i][j]);
+				day = currDate.getDate();
+					if(day == now.getDate() && currDate.getMonth() == now.getMonth()) {
+						calendar += '<td class="today">' + day + '</td>';
+					} else if(currDate < now) {
+						calendar += '<td class="past"><s>' + day + '</s></td>';
+					} else if(j == 0) {
+						calendar += '<td>' + day + '</day>';
+					} else if(currDate.getMonth() > now.getMonth() && currDate.getYear() == now.getYear()
+						|| currDate.getMonth() < now.getMonth() && currDate.getYear() < now.getYear()) {
+						calendar += '<td class="moderate">' + day + '</td>';
+					} else { calendar += '<td class="light">' + day + '</td>'; }
+				}
+				calendar += '</tr>';
+			}
+			document.getElementById('calendar').innerHTML = calendar;
+		}
+                
+        Date.prototype.addDays = function (n) {
+			var time = this.getTime();
+			var changedDate = new Date(time + (n * 24 * 60 * 60 * 1000));
+			this.setTime(changedDate.getTime());
+			return this;
+	}
