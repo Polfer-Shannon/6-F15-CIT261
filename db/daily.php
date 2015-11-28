05219dd689c32fa64c43a28288d9685c3b25f8ff
zvakanaka <quintonish@gmail.com>
2015-11-27 16:26:29 -0800

    Display get params on page

--- db/daily.php
+++ db/daily.php
@@ -1,3 +1,5 @@
 <?php
-echo 'This will call db based on get parameters to load the schedule';
+echo 'This will call db based on get parameters to load the schedule<br/>';
+echo $_GET['day'].'<br/>';
+echo $_GET['when'].'<br/>';
 ?>
