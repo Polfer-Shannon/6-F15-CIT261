c8e5576a246dc8f8bb88d1e6067a9a46fa88e620
zvakanaka <quintonish@gmail.com>
2015-11-27 21:14:39 -0800

    Database setup script created. Please suggest improvements and restructuring needs.

---
+++ db/sched.sql
@@ -0,0 +1,50 @@
+-- TODO: Foreign Keys (will add later once all agree on a design)
+
+-- create user and password
+CREATE USER 'howtote5_php'@'localhost' IDENTIFIED BY 'password';
+
+-- create database named sched
+create database sched;
+use sched;
+
+GRANT SELECT, CREATE, INSERT, DELETE, UPDATE, ALTER ON sched.* TO 'howtote5_php'@'localhost';
+
+CREATE TABLE user
+(
+user_id int NOT NULL AUTO_INCREMENT,
+user_name varchar(255) NOT NULL,
+first_name varchar(255),
+last_name varchar(255),
+email varchar(255),
+PRIMARY KEY (user_id)
+);
+
+CREATE TABLE appointment
+(
+appointment_id int NOT NULL AUTO_INCREMENT,
+user_id int NOT NULL,
+appointment_time date NOT NULL,
+appointment_place varchar(255),
+PRIMARY KEY (appointment_id)
+);
+
+CREATE TABLE admin
+(
+admin_id int NOT NULL AUTO_INCREMENT,
+user_id int NOT NULL,
+business_open time NOT NULL,
+business_close time NOT NULL,
+appointment_place varchar(255),
+PRIMARY KEY (admin_id)
+);
+
+CREATE TABLE admin_hours
+(
+admin_hours_id int NOT NULL AUTO_INCREMENT,
+admin_id int NOT NULL,
+start_time time NOT NULL,
+end_time time NOT NULL,
+unavailable boolean,
+day_of_week varchar(8),
+PRIMARY KEY (admin_hours_id)
+);
