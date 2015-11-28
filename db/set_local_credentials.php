19ea451c0df2bb3d8d131f204429150b6cdeace4
adam quinton <quince@raspberrypi>
2015-11-28 14:32:44 -0800

    Ignore password file

--- db/set_local_credentials.php
+++
@@ -1,9 +0,0 @@
-<?php //set_local_credentials.php
-//show some dubug
-//error_reporting(E_ALL);
-//ini_set("display_errors", "On");
-
-$dbHost = "127.0.0.1";
-$dbUser = "howtote5_php";
-$dbPassword = "";
-?>
