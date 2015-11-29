a218d712cdab473a09fc401d70a2393e33783bb1
Adam Quinton <adam@adam-Inspiron-1564>
2015-11-28 18:00:14 -0700

    Link click to event- sloppy for now

--- js/daily.js
+++ js/daily.js
@@ -1,6 +1,39 @@
 // build the daily availability pane
 function getDaily(day, month, year) {
-	var daily;
+	//var daily;
+
+	var user = localStorage.getItem('user');
+	if (user == null) {
+		user = 'adam';
+	}
+	var jsonString = {day: day,
+                      month: month,
+                      year: year,
+                      user: user
+                     };
+console.log(jsonString);
+console.log(JSON.stringify(jsonString));
+	var stringified = JSON.stringify(jsonString);
 	
-	document.getElementById('daily').innerHTML = daily;
+    //call database to insert
+    database(stringified, 'db/web_service.php');
+
+//TODO: populate this object by ajax requests
+	//document.getElementById('daily').innerHTML = daily;
+}
+
+//call a php file that queries db, then display change on page
+function database(stringified, url) {
+  var http = new XMLHttpRequest(); 
+  http.open("POST", url, true);
+  http.setRequestHeader("Content-type", "application/json; charset=utf-8");
+  http.onreadystatechange = function() {
+    if (http.readyState == 4 && http.status == 200) {
+    	//response
+    	var data = (http.responseText);
+    	console.log(data);
+        document.getElementById('daily').innerHTML = data;
+      }
+  }
+  http.send(stringified);
 }
