33401f8c35f8ee1547a84bcdfe73063d726f409d
Adam Quinton <adam@adam-Inspiron-1564>
2015-11-28 17:59:34 -0700

    Get results from db for appointments on that day

--- db/web_service.php
+++ db/web_service.php
@@ -1,26 +1,29 @@
 <?php
-echo 'This will call db based on get parameters to load the schedule<br/>';
-echo $_GET['day'].'<br/>';
-echo $_GET['when'].'<br/>';
-?><?php
-//insert_json_db.php
-$cleanData = $HTTP_RAW_POST_DATA;
-//$cleanData = json_decode($postedData, true);
+$postedData = $HTTP_RAW_POST_DATA;
+$cleanData = json_decode($postedData, true);
+$date = $cleanData["year"]."-".$cleanData["month"]."-".$cleanData["day"];
+$user = $cleanData["user"];
+
 require 'load_db.php';
 try {
   $db = loadDB();
-  $query = 'select fans from upvote where article_id = :articleId';
-  $statement = $db->prepare($query);
-  $statement->bindParam(':articleId', $cleanData);
-
-  $statement->execute();
-  $result = $statement->fetch();
-  //result
-  echo $result["fans"];
+  //TODO: load appointments, otherwise display blanks (currently loading all appointments for day)
+  //TODO: Do not constrain by user, but do show which user
+  $query = 'select appointment_time, appointment_date from appointment where appointment_date = :date and user_id = (select user_id from user where name = :user)'; 
+  $stmnt = $db->prepare($query);
+  $stmnt->bindParam(':date', $date);
+  $stmnt->bindParam(':user', $user);
+  $stmnt->execute();
+  while($row = $stmnt->fetch())
+  {
+    echo $row['appointment_date']." Time:";
+    echo $row['appointment_time']." ";
+  }
+  echo "SOMETHING";
 }
 catch (Exception $ex)
 {
-	echo "Error with DB. ";//.$ex;
+	echo "Error with DB. ".$ex;
 	die();
 };
 
